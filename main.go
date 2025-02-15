904da877001b59d5fa55e90123fdd8989c7e52d0
JP Appel <jeanpierre.appel01@gmail.com>
2025-02-14 22:00:38 -0500

    Add cli flags and seperate rooms

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/binary"
+	"flag"
 	"fmt"
-	"log"
+	"html/template"
 	"log/slog"
 	"net/http"
+	"os"
 	"sync"
 
 	"github.com/gorilla/websocket"
@@ -19,6 +21,10 @@ const (
 	PAUSED
 	LOADING
 )
+
+var tmpl *template.Template
+var logger *slog.Logger
+var rooms map[string]*sync.Map
 
 // PERF: unused space in struct
 type inMsg struct {
@@ -41,26 +47,38 @@ func (m inMsg) String() string {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello World!</h1>")
+	tmpl.Execute(w, nil)
+}
+
+func script(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "client.js")
 }
 
 var upgrader = websocket.Upgrader{}
-var shared *sync.Map
 
 func barrel(w http.ResponseWriter, r *http.Request) {
+	roomName := r.PathValue("roomName")
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		slog.Error(err.Error())
+		logger.Error(err.Error())
 	}
 	defer c.Close()
 
+	room, ok := rooms[roomName]
+	if !ok {
+		rooms[roomName] = &sync.Map{}
+		room = rooms[roomName]
+	}
+
 	remoteAddr := c.RemoteAddr().String()
-	slog.Info("Opened web socket connection", slog.String("remote", remoteAddr))
+	defer room.Delete(remoteAddr)
+	logger.Info("Opened web socket connection", slog.String("roomName", roomName), slog.String("remote", remoteAddr))
 
 	for {
+		// TODO: check message type for errors
 		_, message, err := c.ReadMessage()
 		if err != nil {
-			slog.Error("Error while reading", slog.Any("error", err))
+			logger.Error("Error while reading", slog.Any("error", err))
 			break
 		}
 		// parse message
@@ -69,40 +87,73 @@ func barrel(w http.ResponseWriter, r *http.Request) {
 			playerState: PlayerState(message[2]),
 		}
 
-		slog.Info("recieved message", slog.String("msg", msg.String()))
-		shared.Store(c.RemoteAddr(), msg)
+		logger.Debug("recieved message", slog.String("msg", msg.String()))
+		room.Store(remoteAddr, msg)
 
-		buf := make([]byte, 0, 128)
-		shared.Range(func(k, v any) bool {
+		buf := make([]byte, 0)
+		room.Range(func(k, v any) bool {
 			msg := v.(inMsg)
-			binary.BigEndian.AppendUint16(buf, msg.offset)
+			buf = binary.BigEndian.AppendUint16(buf, msg.offset)
 			buf = append(buf, byte(msg.playerState))
-			buf = append(buf, []byte(remoteAddr)...)
+			// buf = append(buf, []byte(remoteAddr)...)
 			buf = append(buf, byte(0)) // NOTE: null byte delimits clients
 			return true
 		})
 
 		if c.WriteMessage(2, buf) != nil {
-			slog.Error("Error while writting", slog.Any("error", err))
+			logger.Error("Error while writting", slog.Any("error", err))
 			break
 		}
 	}
-
-	shared.Delete(remoteAddr)
+	logger.Info("Closing web socket connection", slog.String("remote", remoteAddr))
 }
 
 func main() {
-	const HOSTNAME string = "localhost"
-	const PORT int = 8080
-	addr := fmt.Sprintf("%s:%d", HOSTNAME, PORT)
+	port := flag.Int("port", 8080, "port to listen on")
+	hostname := flag.String("hostname", "localhost", "hostname to listen on")
+	loglvl := flag.String("l", "warn", "log level (debug, info, warn, error)")
 
-	shared = &sync.Map{}
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Options:")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	loggerOpts := new(slog.HandlerOptions)
+	switch *loglvl {
+	case "debug":
+		loggerOpts.Level = slog.LevelDebug
+		loggerOpts.AddSource = true
+	case "info":
+		loggerOpts.Level = slog.LevelInfo
+	case "warn":
+		loggerOpts.Level = slog.LevelWarn
+	case "error":
+		loggerOpts.Level = slog.LevelError
+	default:
+		panic(fmt.Sprintf("Unkown log level %s", *loglvl))
+	}
+	logger = slog.New(slog.NewTextHandler(os.Stdout, loggerOpts))
+
+	addr := fmt.Sprintf("%s:%d", *hostname, *port)
+
+	rooms = make(map[string]*sync.Map)
+
+	// parse templtes
+	var err error
+	tmpl, err = template.ParseFiles("templates/index.html")
+	if err != nil {
+		logger.Error("Unable to parse templates", slog.String("Error", err.Error()))
+		return
+	}
 
 	mux := http.NewServeMux()
+	mux.HandleFunc("/barrel/{roomName}", barrel)
+	mux.HandleFunc("/client.js", script)
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/barrel", barrel)
 
-	slog.Info("Starting server", slog.String("bindAddress", addr))
+	logger.Info("Starting server", slog.String("bindAddress", addr))
 
-	log.Fatal(http.ListenAndServe(addr, mux))
+	logger.Info(http.ListenAndServe(addr, mux).Error())
 }
